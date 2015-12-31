5babd7c5942cc00a53d6aa122ea42a7e08dffc61
sunny <singhsu@advisory.com>
2015-12-31 17:39:24 -0500

    committing test with network test

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ with picamera.PiCamera() as camera:
     connection = server_socket.accept()[0].makefile('wb')
     try:
         camera.start_recording(connection, format='h264')
-        camera.wait_recording(60)
+        camera.wait_recording(600)
         camera.stop_recording()
     finally:
         connection.close()
